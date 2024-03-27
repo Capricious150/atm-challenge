c475093fdf8127316d5cf4302829ae71eaf1a9c6
Capricious150 <austinandrews89@gmail.com>
2024-03-27 11:21:06 -0600

    Lunch break

--- server/routes/appRoutes.js
+++ server/routes/appRoutes.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import pg from 'pg';
 export const router = express.Router();
 
+const client = new pg.Client({
+    host: 'localhost',
+    port: '5432',
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
+})
+
 router.post('/login/:id', async (req, res) => {
-    console.log("Entered POST")
-    res.status(200).send("Stay Golden Ponyboy")
+
+    try {
+
+    } catch (err) {
+        
+        res.status(200).send("Stay Golden Ponyboy")
+    }
 })
