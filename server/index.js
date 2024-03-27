83cdbd5528b436ee571c96bcc28c835552722317
Capricious150 <austinandrews89@gmail.com>
2024-03-27 08:24:45 -0600

    Set up very simple Express server to get started, updated env

--- server/index.js
+++ server/index.js
@@ -1,11 +1,49 @@
 //Very simple ExpressJS server to get started
-
 import express from "express"; 
-const app = express();
-const port = 3000;
+import dotenv from "dotenv";
+import pg from 'pg';
+dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send("I am listening")
+const pool = new pg.Pool({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT,
+    host: 'localhost',
+})
+
+const client = new pg.Client({
+    user: 'austin',
+    password: 'test',
+    database: 'challenge',
+    port: '5432',
+    host: '127.0.0.1'
+})
+
+const app = express();
+const port = '3000';
+
+app.get('/', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM accounts');
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    }
+    
+})
+app.get('/c', async (req, res) => {
+    try {
+        console.log("Fetching")
+        await client.connect()
+        const result = await client.query('SELECT * FROM accounts'); 
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    }
+    
 })
 
 app.listen(port, () => {
